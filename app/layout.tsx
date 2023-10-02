b93099ff8cca90d02c07e5e1b1452f78d16e688f
TW-IT(WEB)-Jeff Chao <jeff.jhao@shoalter.com>
2023-10-02 15:00:59 +0800

    refactor: test ga setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <Script id="ga">{`(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+            const f = d.getElementsByTagName(s)[0];
+            const j = d.createElement(s);
+            const dl = l !== 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', ${process.env.NEXT_PUBLIC_GTM_ID})`}</Script>
+      </head>
+      <body>
+        <iframe
+          title="gtm"
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <main>
+      <h1>Hello, Next.js!</h1>
+      <button id="test" onClick={() => console.log("1")}>
+        1
+      </button>
+      <button id="aaa" onClick={() => console.log("2")}>
+        2
+      </button>
+      <button id="test2" onClick={() => console.log("3")}>
+        3
+      </button>
+      <button id="bbb" onClick={() => console.log("4")}>
+        4
+      </button>
+      <button id="ccc" onClick={() => console.log("5")}>
+        5
+      </button>
+    </main>
+  );
 }
