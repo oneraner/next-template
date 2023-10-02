6c7e3c8921276a20377ebb8192107ef46618afb7
TW-IT(WEB)-Jeff Chao <jeff.jhao@shoalter.com>
2023-10-02 15:08:02 +0800

    fix: type error

--- src/stories/doubleSlider/doubleSlider.stories.ts
+++ src/stories/doubleSlider/doubleSlider.stories.ts
@@ -1,23 +1,23 @@
-import type { Meta, StoryObj } from "@storybook/react";
+// import type { Meta, StoryObj } from "@storybook/react";
 
-import { DoubleSlider } from "./doubleSlider";
+// import { DoubleSlider } from "./doubleSlider";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-const meta: Meta<typeof DoubleSlider> = {
-  title: "Components/DoubleSlider",
-  component: DoubleSlider,
-  tags: ["autodocs"],
-  argTypes: {},
-};
+// // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+// const meta: Meta<typeof DoubleSlider> = {
+//   title: "Components/DoubleSlider",
+//   component: DoubleSlider,
+//   tags: ["autodocs"],
+//   argTypes: {},
+// };
 
-export default meta;
-type Story = StoryObj<typeof DoubleSlider>;
+// export default meta;
+// type Story = StoryObj<typeof DoubleSlider>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Main: Story = {
-  args: {
-    min: 0,
-    max: 50000,
-    defaultValue: [0, 100000],
-  },
-};
+// // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+// export const Main: Story = {
+//   args: {
+//     min: 0,
+//     max: 50000,
+//     defaultValue: [0, 100000],
+//   },
+// };
